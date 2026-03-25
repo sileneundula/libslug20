f08bc5a97c4d1234a72822ff9235608dbce4d8c3
silene0259 <158784347+sileneundula@users.noreply.github.com>
2026-03-25 10:37:24 -0400

    GUI Work
    03/25/2026: Worked on lots of GUI work, getting the basic layout to work and the page to load in dynamically.

--- app/openinternetcryptography/src/routes/+layout.svelte
+++ app/openinternetcryptography/src/routes/+layout.svelte
@@ -4,10 +4,23 @@
 
 <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
  -->
+
 </head>
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+<div id="hero">
+<div class="hero-body section">
+    <h1 class="title">OpenInternetCryptography</h1>
+    <h2 class="subtitle">This program contains functionality for the Open Internet Project.<br><i>OpenInternetId:</i> <br> <i>Current-Identity:</i> <br>  </h2>
+    <!-- Add For Current Identity Username and Key Checksum -->
+</div>
+</div>
+<section class="section">
 <nav class="navbar">
 <div class="navbar-brand">
-    <button class="button is-warning">OpenInternetCryptography</button>
+    <a class="navbar-item">Liberatø⨎</a>
 </div>
     <div class="navbar-item">
 	    <a class="navbar-item" href="/home">Home</a>
@@ -30,12 +43,28 @@
     </a>
     </div>
     <div class="navbar-end">
-        <a class="navbar-item navbar-link">Identity</a>
+    <div class="navbar-item has-dropdown is-hoverable">
+        <a id="oint-identity" class="navbar-link">Identity</a>
+        <div class="navbar-dropdown">
+        <a class="navbar-item">
+            Switch Accounts
+        </a>
+        <a class="navbar-item">
+            Add Account
+        </a>
+        </div>
+        </div>
+        <!-- Add Up To 8 accounts -->
     </div>
 </nav>
+</section>
+<footer class="footer">
+<div class="content has-text-centered">
+<p><b>OpenInternetCryptography</b> from Liberatø⨎&#123;&#125; Suite | Built by "SundialSource" Under WeSpeakInCursive.xyz</p>
+</div>
+</footer>
 
-<script>
-	let { children } = $props();
-</script>
+
+
 
 {@render children()}
---
+++ app/openinternetcryptography/src/routes/initial-generation/+page.svelte
@@ -0,0 +1,6 @@
+<section class="section">
+<h1 class="title">Liberatø⨎&#123;Identity-Generation&#125;</h1>
+<p class="subtitle">This is the initial generation of an identity for the liberato ecosystem and other connected entities. It involves generating keypairs for identity and syncing it with the network.</p>
+<input class="input" type="text" placeholder="Liberatø⨎&#123;&#125; - Identity Local Name" />
+</section>
+    
